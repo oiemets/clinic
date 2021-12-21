2fa3a77cc2507f47e6d3d7f955697057a16505cd
olek <oleksandr.yemets@itrexgroup.com>
2021-12-21 02:25:03 +0200

    [fix]: removing options array sorting from select resolutions

--- src/modules/Patient/PatientResolutions/components/SelectResolutions/index.tsx
+++ src/modules/Patient/PatientResolutions/components/SelectResolutions/index.tsx
@@ -12,23 +12,17 @@ type SelectResolutionsProps = {
 	valueHandler: (name: any) => void;
 };
 
-type Options = { value: string; label: string }[];
-
-const selectResolutionsOptions = {
-	firstName: { value: 'firstName', label: 'First Name' },
-	lastName: { value: 'lastName', label: 'Last Name' },
-	visitDate: { value: 'visitDate', label: 'Date' },
-	nextAppointmentDate: { value: 'nextAppointmentDate', label: 'Next Visit' },
-};
+const options = [
+	{ value: 'firstName', label: 'First Name' },
+	{ value: 'lastName', label: 'Last Name' },
+	{ value: 'visitDate', label: 'Date' },
+	{ value: 'nextAppointmentDate', label: 'Next Visit' },
+];
 
 export const SelectResolutions: React.FC<SelectResolutionsProps> = ({
 	sortBy,
 	valueHandler,
 }) => {
-	const add: Options = Object.values(_.omit(selectResolutionsOptions, sortBy));
-
-	const options: Options = [selectResolutionsOptions[sortBy], ...add];
-
 	return (
 		<SelectResolutionsWrapper>
 			<SelectResolutionsTitle>Sort by:</SelectResolutionsTitle>
